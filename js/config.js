3a033e73d71ed85b514053f2ee46fbf130fdfcc3
Karthik M S <41919594+karthiknaidu1805@users.noreply.github.com>
2025-04-27 17:59:48 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Udxm9Ez4a', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7h3m13s2ccg8k25evf3srk2sdk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
